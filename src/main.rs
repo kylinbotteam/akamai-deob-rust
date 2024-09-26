af6fc974f4abfcc64bb5986289abb0e301af5b01
Zeeker <13848632+zeeker999@users.noreply.github.com>
2024-09-26 16:17:07 +0800

    Initial import

---
+++ src/main.rs
@@ -0,0 +1,90 @@
+use swc_common::{sync::Lrc, Globals, SourceMap};
+use swc_ecma_ast::*;
+use swc_ecma_parser::{Parser, StringInput, Syntax};
+use swc_ecma_transforms::pass::noop;
+use swc_ecma_visit::{Fold, VisitMut, visit_mut_obj_and_computed, VisitMutWith};
+
+struct Deobfuscator;
+
+impl VisitMut for Deobfuscator {
+    fn visit_mut_ident(&mut self, ident: &mut Ident) {
+        // Simple logic to rename obfuscated variable names
+        if ident.sym.starts_with('_') {
+            ident.sym = format!("var_{}", &ident.sym[1..]).into();
+        }
+    }
+
+    fn visit_mut_var_declarator(&mut self, var: &mut VarDeclarator) {
+        // You can add more transformations for variable declarations here
+        var.visit_mut_children_with(self);
+    }
+
+    fn visit_mut_expr(&mut self, expr: &mut Expr) {
+        // Simplify complex expressions here
+        expr.visit_mut_children_with(self);
+    }
+}
+
+fn main() {
+    // Input: Obfuscated JavaScript code as a string
+    let code = r#"
+        function _0xabc(_0x123, _0x456) {
+            return _0x123 + _0x456;
+        }
+        const _0x789 = _0xabc(1, 2);
+    "#;
+
+    let cm: Lrc<SourceMap> = Default::default();
+    let globals = Globals::new();
+
+    let fm = cm.new_source_file(
+        swc_common::FileName::Custom("input.js".into()),
+        code.into(),
+    );
+
+    let lexer = swc_ecma_parser::lexer::Lexer::new(
+        Syntax::Es(Default::default()),
+        swc_ecma_parser::EsVersion::Es2020,
+        StringInput::from(&*fm),
+        None,
+    );
+
+    let mut parser = Parser::new_from(lexer);
+
+    let module = parser
+        .parse_module()
+        .expect("Failed to parse the JavaScript code.");
+
+    let mut deobfuscator = Deobfuscator;
+
+    // Apply the deobfuscation transformation
+    let mut transformed_module = module.clone();
+    transformed_module.visit_mut_with(&mut deobfuscator);
+
+    // Code generation (pretty print the transformed AST back to JavaScript)
+    let codegen_config = swc_ecma_codegen::Config {
+        minify: false,
+        ..Default::default()
+    };
+
+    let mut buf = vec![];
+    {
+        let mut emitter = swc_ecma_codegen::Emitter {
+            cfg: codegen_config,
+            cm: cm.clone(),
+            wr: Box::new(swc_ecma_codegen::text_writer::JsWriter::new(
+                cm.clone(),
+                "\n",
+                &mut buf,
+                None,
+            )),
+            comments: None,
+        };
+
+        emitter.emit_module(&transformed_module).unwrap();
+    }
+
+    let output_code = String::from_utf8(buf).unwrap();
+
+    println!("Transformed code:\n{}", output_code);
+}
