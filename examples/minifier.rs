9006386b5ce79454642f544e80c4029009824f47
Zeeker <13848632+zeeker999@users.noreply.github.com>
2024-10-05 20:38:48 +0800

    Added some examples

---
+++ examples/minifier.rs
@@ -0,0 +1,51 @@
+#![allow(clippy::print_stdout)]
+use std::path::Path;
+
+use oxc_allocator::Allocator;
+use oxc_codegen::CodeGenerator;
+use oxc_minifier::{CompressOptions, Minifier, MinifierOptions};
+use oxc_parser::Parser;
+use oxc_span::SourceType;
+use pico_args::Arguments;
+
+// Instruction:
+// create a `test.js`,
+// run `cargo run -p oxc_minifier --example minifier` or `just example minifier`
+
+fn main() -> std::io::Result<()> {
+    let mut args = Arguments::from_env();
+
+    let name = args.subcommand().ok().flatten().unwrap_or_else(|| String::from("test.js"));
+    let mangle = args.contains("--mangle");
+    let twice = args.contains("--twice");
+
+    let path = Path::new(&name);
+    let source_text = std::fs::read_to_string(path)?;
+    let source_type = SourceType::from_path(path).unwrap();
+
+    let mut allocator = Allocator::default();
+    let printed = minify(&allocator, &source_text, source_type, mangle);
+    println!("{printed}");
+
+    if twice {
+        allocator.reset();
+        let printed2 = minify(&allocator, &printed, source_type, mangle);
+        println!("{printed2}");
+        println!("same = {}", printed == printed2);
+    }
+
+    Ok(())
+}
+
+fn minify(
+    allocator: &Allocator,
+    source_text: &str,
+    source_type: SourceType,
+    mangle: bool,
+) -> String {
+    let ret = Parser::new(allocator, source_text, source_type).parse();
+    let mut program = ret.program;
+    let options = MinifierOptions { mangle, compress: CompressOptions::default() };
+    let ret = Minifier::new(options).build(allocator, &mut program);
+    CodeGenerator::new().with_mangler(ret.mangler).build(&program).source_text
+}
